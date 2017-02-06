e491ca9886604af4fa1235209b0577ebc0ee0555
ITSportGuys <christian@itsupportguys.com>
2017-02-06 14:05:00 -0800

    Revert "Modify setup script to run over existing installation"
    This reverts commit 85a60ab9a259c193d34b8fc366518947d343466b.

--- Setup.ps1
+++ Setup.ps1
@@ -1,15 +1,5 @@
-$alert_file_path = ($Env:SystemRoot + "\LTSvc\scripts\BACKUP_FAILURE_ALERT.txt") #location of file LT looks for
 $error_log_dir = ($Env:ProgramData + "\IT Support Guys\Logs\") #full path to dir containing logs
 $guids = @()
-
-#if jobs are already configured exit script
-if (Test-Path .\CONFIG\jobs.config) {
-    Write-Host "Jobs already configured, exiting!"
-    exit 
-}
-
-#else clear database and log directories|
-Write-Host "Writing jobs.config!"
 
 #clears dirs
 Remove-Item .\DB\*
@@ -19,12 +9,4 @@ Remove-Item .\LOGS\*
 $guids += ( $_.ToString().Substring( (0) , ($_.ToString().Length -4) ))
 }
 
-
-if ($guids.Count -gt 0){
-    #if jobs were found write file
-    $guids | Out-File .\CONFIG\jobs.config
-}
-else {
-    #alert setup failed
-    "Job setup failed -- no logs found!" > $alert_file_path
-}
+$guids | Out-File .\CONFIG\jobs.config
